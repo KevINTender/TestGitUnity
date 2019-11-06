b833ca58440411e544410225bf0e6dd615bd758e
KevinTender <tenderkevin@gmail.com>
2019-11-06 19:08:31 +0300

    first commit
    first commit

---
+++ Assets/ShowSelectedObjects.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowSelectedObjects : MonoBehaviour
+{
+    public Camera myCamera;
+    public GameObject selectItem;
+    public GameObject selectedObject;
+    void Start()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(myCamera.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.collider != null)
+            {
+                if (hit.transform.tag == "unit")
+                {
+                    if(selectedObject != null)
+                    {
+                        selectedObject.GetComponent<Unit>().unselected();
+                        selectedObject = null;
+                    }
+                    selectedObject = hit.transform.gameObject;
+                    selectedObject.GetComponent<Unit>().selected();
+                }
+                if (selectedObject != null && hit.transform.tag == "point")
+                {
+                    selectedObject.GetComponent<Unit>().move(hit.transform.position);
+
+                }
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            selectedObject.GetComponent<Unit>().unselected();
+            selectedObject = null;
+        }
+    }
+    
+    void chouseEvent()
+    {
+
+    }
+    void move()
+    {
+
+    }
+ }
---
+++ Assets/Unit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    Vector3 point;
+    public GameObject pointObject;
+    public GameObject selectedObject;
+    void Start()
+    {
+        point = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, point, Time.deltaTime);
+    }
+    public void move(Vector3 moveVector)
+    {
+        point = moveVector;
+        pointObject.SetActive(true);
+        pointObject.transform.position = point;
+      
+        transform.GetComponent<SpriteRenderer>().flipX = (point.x > transform.position.x)?true:false;
+ 
+    }
+    public void selected()
+    {
+        selectedObject.SetActive (true);
+    }
+    public void unselected()
+    {
+        selectedObject.SetActive(false);
+        pointObject.SetActive(false);
+    }
+}
